6b3f358510595faaeb42533e177805a9eca6dbf1
magnayecristianancheta@gmail.com <magnayecristianancheta@gmail.com>
2021-10-09 09:50:01 +0800

    insert people POC

--- src/routers/profile.ts
+++ src/routers/profile.ts
@@ -3,7 +3,13 @@ import connection from "../connection";
 import { ValidationSchemas } from "../validation_schemas/validation_schemas";
 
 const profileRouter = Router();
-
+// const errorTool = (error: any, res: any) => {
+//     if (error) {
+//         console.log('error')
+//         res.status(400);
+//         return res.send(error.message)
+//     }
+// }
 profileRouter.post('/register', (req, res) => {
     const { error, value } = ValidationSchemas.accountCreation.validate(req.body);
     if (error) {
@@ -21,11 +27,14 @@ profileRouter.post('/register', (req, res) => {
      * 
      */
 
-    
-    // connection.query('SELECT * FROM users WHERE id = ?', [userId], function (error, results, fields) {
-    //     if (error) throw error;
-    //     // ...
-    //   });
+    connection.query(`INSERT INTO Peoples
+    (role, university_id, password, department_id, program_id, school_id, first_name, last_name, middle_name, created_on, status)
+    VALUES
+    ("student",1,"password1",1,1,1,"firstnamefromserver","lastname","middlename", NOW(), "active")`, [], (error, results, fields) => {
+        if (error) throw error;
+        // ...
+
+    });
     console.log(value);
     res.send(`success`)
 })
@@ -35,16 +44,19 @@ profileRouter.put('/', (req, res) => {
 })
 // I don't think schoolId is unique
 profileRouter.get('/:schoolId', (req, res) => {
-    const {schoolId} = req.params;
-
+    const { schoolId } = req.params;
+    /**
+     * get from token the school of the admin
+     * pass the school here
+     */
     res.send('profile new')
-        connection.query('SELECT * FROM Peoples WHERE id = ?', [schoolId], function (error, results, fields) {
+    connection.query('SELECT * FROM Peoples WHERE id = ?', [schoolId], function (error, results, fields) {
         if (error) throw error;
         // ...
         console.log(results)
         console.log('fields')
         console.log(results)
-      });
+    });
 })
 
 
