1dbc59f42debe8c526448361871006be1dc14a37
magnayecristianancheta@gmail.com <magnayecristianancheta@gmail.com>
2021-10-21 15:27:22 +0800

    fixed invalid credential when head admin

--- src/middlewares/admin.ts
+++ src/middlewares/admin.ts
@@ -1,7 +1,9 @@
 const admin = (req: any, res: any, next: any) => {
-    if (req.people.role !== 'admin')
-        return res.status(403).send('Access Denied');
-    next();
+    console.log(`role ${req.people.role}`)
+    if (req.people.role === 'admin' || req.people.role === 'head admin')
+        return next();
+    return res.status(403).send('Access Denied');
+
 }
 
 export default admin;
