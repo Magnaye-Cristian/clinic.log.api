ee48231e53642d5926361b403717c563e79284f8
magnayecristianancheta@gmail.com <magnayecristianancheta@gmail.com>
2021-11-05 18:29:06 +0800

    fix logs getting retrieved for different university

--- src/sql_commands/log.sql.ts
+++ src/sql_commands/log.sql.ts
@@ -8,9 +8,9 @@ export abstract class LogSQL {
     static async getAllByUniversityAndMedicineIsNotNull(day: any, month: any, year: any, university_id: number) {
         const [row] = await (await connection).query(`
         select l.id, l.type, Peoples.school_id, l.type_spec, l.people_id, l.purpose, l.complaint, l.first_name, l.last_name, l.middle_name, l.address, l.timein, l.timeout, l.university_id, l.department, l.medicine from Logs as l LEFT JOIN Peoples on l.people_id = Peoples.id
-         where medicine is not null and day(timeout) = ? and month(timeout) = ? and year(timeout) = ?`
+         where medicine is not null and day(timeout) = ? and month(timeout) = ? and year(timeout) = ? and university_id = ?`
             , [
-                day, month, year
+                day, month, year, university_id
             ])
         console.log(row);
         return row
@@ -19,9 +19,9 @@ export abstract class LogSQL {
     static async getAllByUniversityAndTimeoutIsNotNull(day: any, month: any, year: any, university_id: number) {
         const [row] = await (await connection).query(`
         select l.id, l.type, Peoples.school_id, l.type_spec, l.people_id, l.purpose, l.complaint, l.first_name, l.last_name, l.middle_name, l.address, l.timein, l.timeout, l.university_id, l.department, l.medicine from Logs as l LEFT JOIN Peoples on l.people_id = Peoples.id
-         where timeout is not null and day(timeout) = ? and month(timeout) = ? and year(timeout) = ?`
+         where timeout is not null and day(timeout) = ? and month(timeout) = ? and year(timeout) = ? and university_id = ?`
             , [
-                day, month, year
+                day, month, year, university_id
             ])
         console.log(row);
         return row
